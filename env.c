450f7ab8ae6b5a972245d1425d5a588fdd4253f4
HeoJamong <wlaud0823@naver.com>
2024-09-14 17:36:04 +0900

    refactor: env.c 놈 수정

--- env.c
+++ env.c
@@ -1,92 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheo <jheo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 15:43:36 by jheo              #+#    #+#             */
+/*   Updated: 2024/09/14 17:36:00 by jheo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void    print_env(char **env)
+void	print_env(char **env)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(env[i])
-    {
-        printf("%s\n", env[i]);
-        i++;
-    }
+	i = 0;
+	while (env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
 }
 
-char    **set_env(char **envp)  // 먼저 이 함수 호출해서 환경변수를 다 복사함
+char	**set_env(char **envp)
 {
-    char    **new;
-    int     i;
+	char	**new;
+	int		i;
 
-    i = 0;
-    while (envp[i])
-        i++;
-    new = malloc(sizeof(char *) * (i + 1));
-    if (new == NULL)
-        return (NULL);
-    i = 0;
-    while (envp[i])
-    {
-        new[i] = ft_strdup(envp[i]);
-        i++;
-    }
-    new[i] = NULL;
-    return (new);
+	i = 0;
+	while (envp[i])
+		i++;
+	new = malloc(sizeof(char *) * (i + 1));
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		new[i] = ft_strdup(envp[i]);
+		i++;
+	}
+	new[i] = NULL;
+	return (new);
 }
 
-int check_env(char *str, char *env)
+int	check_env(char *str, char *env)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    // 저장하려고 하는 환경변수와 현재 저장 된 환경변수를 비교하여 이미 존재하는 환경 변수인지 확인
-    while (str[i] && env[i] && (str[i] == env[i]) && (str[i] != '=') && (env[i] != '='))
-        i++;
-    if (i == 0)
-        return (0);
-    if (((str[i] == '=') && (env[i] == '=')))
-        return (1); //같은 이름의 환경 변수가 존재할 때
-    return (0); //존재 하지 않는 환경 변수일 경우
+	i = 0;
+	while (str[i] && env[i] && (str[i] == env[i]) && \
+	(str[i] != '=') && (env[i] != '='))
+		i++;
+	if (i == 0)
+		return (0);
+	if (((str[i] == '=') && (env[i] == '=')))
+		return (1);
+	return (0);
 }
 
-void    new_export(char *str, char **new, int index)
+void	new_export(char *str, char **new, int index)
 {
-    new[index] = ft_strdup(str);
-    new[index + 1] = NULL;
+	new[index] = ft_strdup(str);
+	new[index + 1] = NULL;
 }
 
-int ft_export(char *str, t_cmd *cmd)
+int	ft_export(char *str, t_cmd *cmd)
 {
-    int i;
-    char    **new;
+	int		i;
+	char	**new;
 
-	printf("호출 됐음\n");
-	print_env(cmd->envp);
-    if (str[0] == '=' || str[0] == '\0' || ft_isdigit(str[0])) // 이거 뭐 _랑 더 있는데 추가해야함
-        return (0);
-    i = 0;
-    while((cmd->envp)[i])
-    {
-        if (check_env(str, (cmd->envp)[i]))
-        {
-            //이미 존재하는 이름의 환경변수라면 기존에 할당 된 값을 프리하고 새로 할당
-            free((cmd->envp)[i]);
-            (cmd->envp)[i] = ft_strdup(str);
-            return (1);
-        }
-        i++;
-    }
-    new = malloc(sizeof(char *) * (i + 2)); //하나 더 추가해줘야 하니 공간 2개 증가
-    if (new == NULL)
-        exit(1);
-    i = 0;
-    while (cmd->envp[i]) //무지성 복사후 초기화 (아니 그냥 연결리스트할껄;;;;;)
-    {
-        new[i] = ft_strdup((cmd->envp)[i]);
-        free((cmd->envp)[i]);
-        i++;
-    }
-    new_export(str, new ,i);
-    free(cmd->envp);
-    cmd->envp = new;
-    return (1);
+	if (str[0] == '=' || str[0] == '\0' || ft_isdigit(str[0]))
+		return (0);
+	i = 0;
+	while ((cmd->envp)[i])
+	{
+		if (check_env(str, (cmd->envp)[i]))
+		{
+			free((cmd->envp)[i]);
+			(cmd->envp)[i] = ft_strdup(str);
+			return (1);
+		}
+		i++;
+	}
+	new = malloc(sizeof(char *) * (i + 2));
+	if (new == NULL)
+		exit(1);
+	i = 0;
+	while (cmd->envp[i])
+	{
+		new[i] = ft_strdup((cmd->envp)[i]);
+		free((cmd->envp)[i]);
+		i++;
+	}
+	new_export(str, new, i);
+	free(cmd->envp);
+	cmd->envp = new;
+	return (1);
 }
