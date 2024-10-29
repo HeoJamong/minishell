d01fb218a5ff4dadc27aa51c4db66b83c1d0efa7
Jinseong Choi <jinsecho@c2r18s2.42gyeongsan.kr>
2024-10-29 16:43:13 +0900

    [norm]code수정

--- ms_line_token1.c
+++ ms_line_token1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ms_line_token.c                                    :+:      :+:    :+:   */
+/*   ms_line_token1.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jinsecho <jinsecho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 19:56:11 by jinsecho          #+#    #+#             */
-/*   Updated: 2024/10/29 15:38:28 by jinsecho         ###   ########.fr       */
+/*   Updated: 2024/10/29 16:42:29 by jinsecho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,50 +56,6 @@ static void	line_token_redirect_join(\
 	if (cmd->line_split[*line_i] == NULL)
 		exit (EXIT_FAILURE);
 	free(tmp);
-}
-
-void	line_token_redirect(t_cmd *cmd, char *line, int *line_i, int *i)
-{
-	if (line[*i] == '>')
-	{
-		cmd->line_split[*line_i] = ms_line_tokenizing_str(cmd, line, i);
-		if (line[*i] == '>')
-			line_token_redirect_join(cmd, line, line_i, i);
-	}
-	else
-	{
-		cmd->line_split[*line_i] = ms_line_tokenizing_str(cmd, line, i);
-		if (line[*i] == '<')
-			line_token_redirect_join(cmd, line, line_i, i);
-	}
-	(*line_i)++;
-}
-
-void	line_token_str(t_cmd *cmd, char *line, int *line_i, int *i)
-{
-	char	*tmp;
-
-	cmd->line_split[*line_i] = ms_line_tokenizing_str(cmd, line, i);
-	if (cmd->line_split[*line_i] == NULL)
-		exit (EXIT_FAILURE);
-	while (ft_strchr(" |><", line[*i]) == NULL && line[*i])
-	{
-		if (line[*i] == DOUBLE_QUOTE || line[*i] == SINGLE_QUOTE)
-		{
-			tmp = ms_line_tokenizing_quote(cmd, line, i);
-			if (tmp == NULL)
-			{
-				cmd->line_split[++(*line_i)] = NULL;
-				line_split_free(cmd);
-				return ;
-			}
-		}
-		else
-			tmp = ms_line_tokenizing_str(cmd, line, i);
-		cmd->line_split[*line_i] = ft_strjoin(cmd->line_split[*line_i], tmp);
-		free(tmp);
-	}
-	(*line_i)++;
 }
 
 void	ms_line_tokenizer(t_cmd *cmd, char *line)
