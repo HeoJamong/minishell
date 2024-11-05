6488b43f8df660ece076ac7706a0b54710c8199e
Jinseong Choi <jinsecho@c2r16s3.42gyeongsan.kr>
2024-11-05 16:17:33 +0900

    input_arg x fix

--- main.c
+++ main.c
@@ -228,6 +228,8 @@ int	ms_line_pipe_split(t_cmd *cmd)
 
 	i = 0;
 	k = 0;
+	if (cmd->line_split[i] == NULL)
+		return (1);
 	cmd->pipe_lst = ms_lstnew();
 	tmp = cmd->pipe_lst;
 	tmp->pipe_split = (char **)malloc(sizeof(char *) * (cmd->line_i + 1));
